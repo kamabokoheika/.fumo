ffb398c598e7d34a693ea3a33c5c03f60b80e6c0
かまぼこ陛下 <nuko@kama5.link>
2026-06-15 12:57:23 +0900

    無駄な変数を増やすな

--- fumo.php
+++ fumo.php
@@ -2,6 +2,8 @@
 // .fumoはなんか自分用のやつです
 // 実行する優先度はfumoが上です
 
+$koreha_sekaiiti_mudana_hennsuu_nanode_ijiru_imiga_nainndesu_souieba_nihonngotte_tukaetakke_hennsuu_meini = "使えるわけがないだろだったら一体何に使えるんだ";
+$douyara_suujito_anndaabaa_to_oomoji_komoji_no_eigodake_sita_tukaenairasii = "あらそうだったのね";
 
 // 利用者のお前らがいじるconfig
 
